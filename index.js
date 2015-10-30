dea423f4de738bd3f0c339976fbd2b04c679ddb2
Stanislav Kalashnik <sk@infomir.eu>
2015-10-30 17:30:22 +0200

    replace implementation

--- index.js
+++ index.js
@@ -10,68 +10,27 @@
 
 /* eslint-disable */
 
-if ( !document.documentElement.classList ) {
-	var prototype = Array.prototype,
-		indexOf   = prototype.indexOf,
-		slice     = prototype.slice,
-		push      = prototype.push,
-		splice    = prototype.splice,
-		join      = prototype.join;
-
-	window.DOMTokenList = function ( el ) {
-		this._element = el;
-		if (el.className !== this._classCache) {
-			this._classCache = el.className;
-			if (!this._classCache) { return; }
-			var classes = this._classCache.replace(/^\s+|\s+$/g,'').split(/\s+/),
-				i;
-			for (i = 0; i < classes.length; i++) {
-				push.call(this, classes[i]);
-			}
+if ( !Function.prototype.bind ) {
+	Function.prototype.bind = function ( oThis ) {
+		if ( typeof this !== 'function' ) {
+			// closest thing possible to the ECMAScript 5
+			// internal IsCallable function
+			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
 		}
+
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
+			fNOP = function () {},
+			fBound = function () {
+				return fToBind.apply(this instanceof fNOP && oThis
+						? this
+						: oThis,
+					aArgs.concat(Array.prototype.slice.call(arguments)));
+			};
+
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+
+		return fBound;
 	};
-
-	window.DOMTokenList.prototype = {
-		add: function ( token ) {
-			if(this.contains(token)) { return; }
-			push.call(this, token);
-			this._element.className = slice.call(this, 0).join(' ');
-		},
-
-		contains: function ( token ) {
-			return indexOf.call(this, token) !== -1;
-		},
-
-		item: function ( index ) {
-			return this[index] || null;
-		},
-
-		remove: function ( token ) {
-			var i = indexOf.call(this, token);
-			if (i === -1) {
-				return;
-			}
-			splice.call(this, i, 1);
-			this._element.className = slice.call(this, 0).join(' ');
-		},
-
-		toString: function () {
-			return join.call(this, ' ');
-		},
-
-		toggle: function ( token ) {
-			if (!this.contains(token)) {
-				this.add(token);
-			} else {
-				this.remove(token);
-			}
-			return this.contains(token);
-		}
-	};
-
-	Object.defineProperty(Element.prototype, 'classList', {
-		get: function () {
-			return new window.DOMTokenList(this);
-		}
-	});
 }
